155f60d52de93e4b49ab60a64ad254db65fe0c6a
Wei Wang <periwinkle.david@gmail.com>
2020-08-03 10:52:25 -0400

    fix the non ascii code in .onload.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("Rfastp is a wrapper of fastp project: https://github.com/OpenGene/fastp")
-    packageStartupMessage("Please cite fastp in your publication:")
-    packageStartupMessage("Shifu Chen, Yanqing Zhou, Yaru Chen, Jia Gu; fastp: an ultra-fast all-in-one FASTQ preprocessor, Bioinformatics, Volume 34, Issue 17, 1 September 2018, Pages i884–i890, https://doi.org/10.1093/bioinformatics/bty560")
+    packageStartupMessage("Rfastp is a wrapper of fastp project: https://github.com/OpenGene/fastp\n\nPlease cite fastp in your publication:\nShifu Chen, Yanqing Zhou, Yaru Chen, Jia Gu; fastp: an ultra-fast all-in-one FASTQ preprocessor, Bioinformatics, Volume 34, Issue 17, 1 September 2018, Pages i884-i890, https://doi.org/10.1093/bioinformatics/bty560")
 }

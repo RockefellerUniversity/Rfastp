f8ad763601c137d81b491db3258fc00e1b8896d0
ThomasCarroll <tc.infomatics@gmail.com>
2021-01-22 20:16:42 -0500

    add gencore back and fix windows compile error.

---
+++ src/GENCORE/jsonreporter.h
@@ -0,0 +1,27 @@
+#ifndef JSON_REPORTER_H
+#define JSON_REPORTER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include "options.h"
+#include "stats.h"
+#include "bed.h"
+#include <fstream>
+
+using namespace std;
+namespace GENCORE {
+class JsonReporter{
+public:
+    JsonReporter(Options* opt);
+    ~JsonReporter();
+
+    void report(Stats* preStats, Stats* postStats);
+
+private:
+    Options* mOptions;
+};
+}
+
+#endif
---
+++ src/GENCORE/stats.h
@@ -0,0 +1,63 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <map>
+#include "options.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "bed.h"
+
+#define MAX_SUPPORTING_READS 100
+namespace GENCORE {
+class Stats{
+public:
+    Stats(Options* opt);
+    ~Stats();
+    void addRead(bam1_t* b);
+    void addMolecule(unsigned int supportingReads, bool PE);
+    void addCluster(bool hasMultiMolecule);
+    void print();
+    long getMappedBases();
+    long getMappedReads();
+    long getReads();
+    long getBases();
+    void reportJSON(ofstream& ofs);
+    double getMappingRate();
+    double getDupRate();
+    long getMolecules() {return mMolecule;}
+    double getMismatchRate();
+    void makeGenomeDepthBuf();
+    void makeBedStats(Bed* other = NULL);
+    void statDepth(int tid, int start, int len);
+
+public:    
+    static string list2string(double* list, int size);
+    static string list2string(double* list, int size, long* coords);
+    static string list2string(long* list, int size);
+
+public:
+    long mReadWithMismatches;
+    long mCluster;
+    long mMultiMoleculeCluster;
+    long mMolecule;
+    long mMoleculeSE;
+    long mMoleculePE;
+    long* mSupportingHistgram;
+    Options* mOptions;
+    long mBase;
+    long mBaseMismatches;
+    long mBaseUnmapped;
+    long mRead;
+    long mReadUnmapped;
+    long uncountedSupportingReads;
+    vector<vector<long>> mGenomeDepth;
+    Bed* mBedStats;
+};
+
+}
+#endif
